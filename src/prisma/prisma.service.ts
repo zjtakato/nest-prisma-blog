e2992c3d1495e9486e3e63783408ddfa6127995e
v_vjttzheng <v_vjttzheng@tencnet.com>
2023-01-01 21:20:17 +0800

    🎈 perf: 增加重写prisma的例子
    为了优雅的隐藏prisma查出来的password字段

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,6 +3,28 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  // constructor() {
+  //   super();
+  //   const self = this.user;
+  //   // @ts-ignore
+  //   this.user = {
+  //     ...this.user,
+  //     async findFirst(params) {
+  //       const result = await self.findFirst(params);
+  //       if (result.password) {
+  //         delete result.password;
+  //       }
+  //       return result;
+  //     },
+  //     async findUnique(params) {
+  //       const result = await self.findUnique(params);
+  //       if (result.password) {
+  //         delete result.password;
+  //       }
+  //       return result;
+  //     },
+  //   };
+  // }
   async onModuleInit() {
     await this.$connect();
   }
