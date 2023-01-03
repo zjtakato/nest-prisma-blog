2982aef06d38e2c0c716d57745274e9fd0707773
v_vjttzheng <v_vjttzheng@tencnet.com>
2023-01-03 17:49:17 +0800

    🎈 perf: 修改类型
    将prisma提供的类型都置为可选

--- decorator/state.decorator.ts
+++ decorator/state.decorator.ts
@@ -1,8 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { User } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 
-export const State = createParamDecorator((data: unknown, ctx: ExecutionContext): User => {
+export const State = createParamDecorator((data: unknown, ctx: ExecutionContext): StateDto => {
   const request = ctx.switchToHttp().getRequest();
-
-  return request.state as User;
+  return request.state as StateDto;
 });
+
+export interface StateDto extends Partial<Prisma.UserUncheckedCreateWithoutBlogInput> {}
--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,19 +1,18 @@
-import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { SearchPaginationBase } from 'types/index.dto';
 import { JwtAuthGuard } from 'middleware/jwtAuth.guard';
 import { ApiTags } from '@nestjs/swagger';
 import { BlogService } from './blog.service';
-import { BlogDto } from './index.dto';
-import { State } from 'decorator/state.decorator';
-import { User } from '@prisma/client';
+import { BlogDeleteDto, BlogDto } from './index.dto';
+import { State, StateDto } from 'decorator/state.decorator';
 
 @ApiTags('blog')
 @Controller('blog')
 export class BlogController {
   constructor(private readonly blogService: BlogService) {}
   @Get('getBlogList')
-  async getBlogList(@Query() query: SearchPaginationBase) {
-    const data = await this.blogService.getBlogListByPage(query);
+  async getBlogList(@Query() query: SearchPaginationBase, @State() state: StateDto) {
+    const data = await this.blogService.getBlogListByPage(query, state.id);
     return {
       ret: 0,
       data,
@@ -22,7 +21,7 @@ export class BlogController {
 
   @UseGuards(JwtAuthGuard)
   @Post('createBlog')
-  async createBlog(@Body() body: BlogDto, @State() state: User) {
+  async createBlog(@Body() body: BlogDto, @State() state: StateDto) {
     const { id } = await this.blogService.createBlog({ ...body, userId: state.id });
     return {
       ret: 0,
@@ -33,7 +32,7 @@ export class BlogController {
 
   @UseGuards(JwtAuthGuard)
   @Post('updateBlog')
-  async updateBlog(@Body() { id, ...body }: BlogDto, @State() state: User) {
+  async updateBlog(@Body() { id, ...body }: BlogDto, @State() state: StateDto) {
     await this.blogService.updateBlogById(id, state.id, body);
     return {
       ret: 0,
