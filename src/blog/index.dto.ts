a39f8b840bfb2b2723e6bac7fbe4b19b70b0e5b6
v_vjttzheng <v_vjttzheng@tencnet.com>
2023-01-04 10:44:47 +0800

    📃 docs: dto增加jscDoc comment

--- src/blog/index.dto.ts
+++ src/blog/index.dto.ts
@@ -2,8 +2,10 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Prisma } from '@prisma/client';
 
 export class BlogUniversalDto implements Partial<Prisma.BlogUncheckedCreateInput> {
+  /** user表主键id */
   public userId?: number;
 
+  /** 博客id - '更新/删除'必传 */
   @ApiProperty({
     description: `博客id - '更新/删除'必传`,
     default: 0,
@@ -11,12 +13,15 @@ export class BlogUniversalDto implements Partial<Prisma.BlogUncheckedCreateInput
   })
   public id?: number;
 
+  /** 标题 - '新增/更新'必传 */
   @ApiProperty({
     description: `标题 - '新增/更新'必传`,
     default: '这是标题',
     required: false,
   })
   public title?: string;
+
+  /** 内容 - '新增/更新'必穿 */
   @ApiProperty({
     description: `内容 - '新增/更新'必穿`,
     default: '这是标题',
--- types/index.dto.ts
+++ types/index.dto.ts
@@ -1,12 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class SearchPaginationBase {
+  /** 页数 */
   @ApiProperty({
     description: '当前页数',
     default: 1,
   })
   currentPage?: string;
 
+  /** 页码 */
   @ApiProperty({
     description: '页码',
     default: 10,
