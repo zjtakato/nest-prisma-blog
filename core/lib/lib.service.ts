4376e376b97fa8c5a8422bc53a42dfb0f1f41f1f
zjtakato <zjtakato@gmail.com>
2023-01-09 17:54:06 +0800

    ✨ feat: 随机数lib

--- core/lib/lib.service.ts
+++ core/lib/lib.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class LibService {}
+export class LibService {
+  /**
+   * 随机生成字符串
+   * @param length 字符串的长度，默认为32
+   */
+  randomString(length = 32): string {
+    const str = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += str.charAt(Math.floor(Math.random() * str.length));
+    }
+    return result;
+  }
+}
